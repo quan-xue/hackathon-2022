cd736273b3c123b128ff73f7b7b146702123519f
Quan Xue <xuequan010@gmail.com>
2022-02-21 15:44:48 +0800

    moderator bot demo

--- broadcaster/moderator.py
+++ broadcaster/moderator.py
@@ -14,7 +14,9 @@ logger = logging.getLogger(__name__)
 def screen_message(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     logger.info('Message text: %s', update.message.text)
-    update.message.bot.send_message(int(os.getenv('MODERATOR_USERID')), update.message.text)
+    username = update.message.from_user.username
+    message = f'@{username} sent the following offensive message: \n{update.message.text}'
+    update.message.bot.send_message(int(os.getenv('MODERATOR_USERID')), message)
 
 
 moderator = MessageHandler(Filters.text & ~Filters.command, screen_message)
