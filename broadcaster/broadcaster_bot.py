3e68d0ed9512686560c1ff0bc47af74156134991
xtrntr <kangren.chia@gmail.com>
2022-02-21 14:39:38 +0800

    event aggregation bot + schema change to csv

--- broadcaster/broadcaster_bot.py
+++ broadcaster/broadcaster_bot.py
@@ -7,14 +7,19 @@ Bot for directing new joiners to their group
 
 import logging
 import os
+import requests
 import sqlite3
 import sys
+from dateutil import parser
+from datetime import datetime, timedelta
+from geopy import distance
 
 from telegram import Update, ParseMode
 from telegram.ext import (
     CallbackContext
 )
 from telegram.ext import Updater, CommandHandler
+import pandas as pd
 
 sys.path.append('../')
 from listener.psa_listener import ALL_KAMPONGS
@@ -23,6 +28,7 @@ from moderator import moderator
 
 BOT_TOKEN = os.getenv('PSA_BROADCASTER_BOT_TOKEN')
 BROADCAST_POLL_INTERVAL = 10  # in seconds
+EVENT_AGGREGATION_INTERVAL = int(os.getenv('EVENT_AGGREGATION_INTERVAL'))
 
 # Enable logging
 logging.basicConfig(
@@ -30,6 +36,14 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+CC_LOCATION = pd.read_csv('data/cc_name_coords_link.csv')
+CHAT_ID_LOCATION_MAPPING = {}
+for _, row in CC_LOCATION.iterrows():
+    if not pd.isna(row['chat_id']):
+        chat_id = row['chat_id']
+        lat, lng = row['lat'], row['long']
+        CHAT_ID_LOCATION_MAPPING[chat_id] = (lat, lng)
 
 
 def fetch_and_send(context: CallbackContext):
@@ -50,6 +64,91 @@ def fetch_and_send(context: CallbackContext):
         context.bot.send_message(chat_id=chat_id, text=message, parse_mode=ParseMode.MARKDOWN)
 
 
+def filter_events(events, chat_id):
+    # Sanity checks
+    logger.info(CHAT_ID_LOCATION_MAPPING)
+    filter_count = 0
+
+    dist_mapping = {}
+
+    for event in events:
+        # TODO: better time check
+        now = datetime.now()
+        # TODO: return timezone info in events API response
+        event_time = parser.parse(event['start_time']).replace(tzinfo=None) + timedelta(hours=8)
+        is_within_one_week_ahead = (event_time - now) > timedelta(days=7)
+        if not is_within_one_week_ahead:
+            filter_count += 1
+            continue
+
+        kampong_latlng = CHAT_ID_LOCATION_MAPPING[chat_id]
+        lat, lng = event["lat"], event["lng"]
+        dist = distance.distance(kampong_latlng, (lat, lng)).kilometers
+        dist_mapping[dist] = event
+
+    # TODO: get top 3 by nearest location
+    keys = sorted(dist_mapping.keys())[:3]
+    logger.info(f"Nearest locations: {keys}. Filtered out {filter_count} entries outside of 1 week window.")
+    return [dist_mapping[k] for k in keys]
+
+
+def format_time(time):
+    # TODO: return timezone info in events API response
+    return (parser.parse(time) + timedelta(hours=8)).strftime("%d %b %y, %I:%M %p")
+
+
+def format_events(events):
+    events_str = ["What's cooking in the week ahead? 🕺"]
+    for event in events:
+        name = event["name"]
+        organizer = event["organizer"]
+        address = event["address"]
+        start, end = format_time(event['start_time']), format_time(event['start_time'])
+        description = event["description"]
+        eventstr = (
+            f'🎉 {name} organized by 🙆‍♂ {organizer}\n'
+            f'📍 {address}\n'
+            f'🕔 {start} to {end}\n'
+            f'✉️ {description}\n'
+        )
+
+        if "url" in event and event["url"] is not None:
+            url = event["url"]
+            eventstr += f"{url}\n"
+
+        events_str.append(eventstr)
+
+    event_str_separator = "\n\n"
+
+    return event_str_separator.join(events_str)
+
+
+
+def aggregate_events(context: CallbackContext):
+    kampong = context.job.context['kampong']
+    chat_id = context.job.context['chat_id']
+
+    events = requests.get("http://server:8000/api/events").json()
+    logger.info(f"{events[0]}")
+
+    message = format_events(filter_events(events, chat_id))
+
+    context.bot.send_message(chat_id=chat_id, text=message, parse_mode=ParseMode.MARKDOWN)
+
+
+def broadcast_events(update: Update, context: CallbackContext):
+    logger.info('Broadcast event aggregation cronjob started.')
+    kampong = 'Kolam Ayer'
+    chat_id = update.message.chat_id
+    job_context = {
+        'chat_id': chat_id,
+        'kampong': kampong
+    }
+    logger.info(f"Event aggregation for kampong {kampong} with chat_id: {chat_id}")
+
+    context.job_queue.run_repeating(aggregate_events, interval=EVENT_AGGREGATION_INTERVAL, context=job_context)
+
+
 def broadcast_start(update: Update, context: CallbackContext):
     logger.info('Broadcast polling started.')
     kampong = 'Kolam Ayer'
@@ -57,7 +156,7 @@ def broadcast_start(update: Update, context: CallbackContext):
         'chat_id': update.message.chat_id,
         'kampong': kampong
     }
-    logger.info(f"Kampong: {kampong}")
+    logger.info(f"Broadcasting for Kampong: {kampong}")
 
     context.job_queue.run_repeating(fetch_and_send, interval=BROADCAST_POLL_INTERVAL, context=job_context)
 
@@ -77,6 +176,7 @@ def main() -> None:
     dispatcher.add_handler(moderator)
 
     # add kang ren's event broadcaster here
+    dispatcher.add_handler(CommandHandler('broadcaststart', broadcast_events))
 
     # Start the Bot
     updater.start_polling()
